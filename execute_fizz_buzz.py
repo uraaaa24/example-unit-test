e60f1a31fa5c7711a5f12305d2a1aacf1466ffb9
Yuto Urashima <yuto.urashima24@gmail.com>
2023-12-25 16:26:21 +0900

    fizzbuzzのテスト実装

--- execute_fizz_buzz.py
+++ execute_fizz_buzz.py
@@ -1,4 +1,10 @@
 def fizz_buzz(num: int) -> str:
+  if num % 3 == 0 and num % 5 == 0:
+    fizz_buzz = "FizzBuzz"
+    return fizz_buzz
   if num % 3 == 0:
     return "Fizz"
+  if num % 5 == 0:
+    buzz = "Buzz"
+    return buzz
   return str(num)
--- test_execute_fizz_buzz.py
+++ test_execute_fizz_buzz.py
@@ -5,7 +5,7 @@ from execute_fizz_buzz import fizz_buzz
 
 @pytest.mark.parametrize(
     ["test_num"],
-    [[3], [6], [9], [12], [15], [18], [21], [24], [27]]
+    [[3], [6], [9], [12], [18], [21], [24], [27]]
 )
 
 # test~と書いてあったら、pytestがテストの実行対象として認識してくれる
@@ -19,4 +19,26 @@ def test_fizz_buzz_multiple_of_3(test_num) -> None:
 
   # 検証
   expected = "Fizz"
+  assert result == expected
+
+def test_fizz_buzz_multiple_of_5() -> None:
+  # 準備
+  num = 5
+
+  # 実行
+  result = fizz_buzz(num)
+
+  # 検証
+  expected = "Buzz"
+  assert result == expected
+
+def test_fizz_buzz_multiple_of_3_5() -> None:
+  # 準備
+  num = 15
+
+  # 実行
+  result = fizz_buzz(num)
+
+  # 検証
+  expected = "FizzBuzz"
   assert result == expected
